0e9799cc7aefdab620b0a96a79015902008f0192
ComputerSpieler <36859459+computerspieler@users.noreply.github.com>
2025-12-12 22:11:02 +0100

    Fix build script
    Now it will stop if the ndk fails

--- utils/build.sh
+++ utils/build.sh
@@ -5,23 +5,17 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
-cd /opt/ndk
-
-if [ ! -d "./apps/$1" ]; then
+if [ ! -d "/opt/ndk/apps/$1" ]; then
     echo "Please give a valid project name"
     exit 1
 fi
 
-make APP="$1"
-
-cd apps/$1
-build_xml_path=$(find . -name "build.xml" -type f)
+APP_FOLDER="/opt/ndk/apps/$1"
+build_xml_path=$(find "$APP_FOLDER" -name "build.xml" -type f)
 if [ -z "$build_xml_path" ]; then
     echo "Your project is not initialized, please run"
     echo " init [YOUR PROJECT NAME]"
     exit 1
 fi
 
-cd $(dirname "$build_xml_path")
-
-ant $2
+(cd /opt/ndk; make APP="$1") && (cd $(dirname "$build_xml_path"); ant "$2")
